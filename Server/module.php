5a5e73f8ee47f3a1fa1316fae88357a76f7ffc93
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-01-28 16:56:40 +0100

    Add files via upload

--- Server/module.php
+++ Server/module.php
@@ -1,1 +1,35 @@
+<?php
+	class BHKW extends IPSModule {
 
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+
+
+	
+		private function GetVarID($Ident)
+		{
+					
+			$StatusID = $this->GetIDForIdent("KirschStatus");
+			IPS_LogMessage("BHKW statePP StatusID", $StatusID);
+			
+			rIdent("Drosselklapenstellung")  , $ScriptData['V2']);
+			
+		}
+	}
